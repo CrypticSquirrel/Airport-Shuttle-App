c93c642a51cb96ccbf10e6de8ea8321777d83f07
CrypticSquirrel <brejae@andrews.edu>
2020-04-30 08:02:40 -0400

    ticketing is working

---
+++ docs/scripts/ticketing.js
@@ -0,0 +1,57 @@
+/* ---------------------------- Global Variables ---------------------------- */
+
+let isParkingCovered = true;
+
+function nice(json) {
+    // create Tabulator on DOM element with id "example-table"
+    const table = new Tabulator('#example-table', {
+        data: [json], // assign data to table
+        layout: 'fitColumns',
+        columns: [
+            // Define Table Columns
+            { title: 'VIN', field: 'VIN' },
+            { title: 'check_in_date', field: 'check_in_date' },
+            { title: 'check_in_time', field: 'check_in_time' },
+            { title: 'lot_id', field: 'lot_id' },
+            { title: 'rate_code', field: 'rate_code' },
+        ],
+    });
+}
+
+/* ---------------------------- JQuery Click Handlers ---------------------------- */
+
+$(document).ready(function() {
+    $('#print').click(() => {
+        const VIN = $('#vin').val();
+        if (VIN) {
+            fetch('/handleTicket', {
+                method: 'POST',
+                body: JSON.stringify({
+                    parkingType: isParkingCovered ? 'covered' : 'uncovered',
+                    VIN,
+                }),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+                .then(response => response.json())
+                .then(json => {
+                    $('#HomeDisplay').hide();
+                    $('#DataDisplay').show();
+                    console.log(json);
+                    nice(json);
+                });
+        }
+    });
+
+    $('#covered-parking').click(() => {
+        $('#covered-parking').toggleClass('active');
+        $('#uncovered-parking').toggleClass('active');
+        isParkingCovered = true;
+    });
+    $('#uncovered-parking').click(() => {
+        $('#uncovered-parking').toggleClass('active');
+        $('#covered-parking').toggleClass('active');
+        isParkingCovered = false;
+    });
+});
--- index.js
+++ index.js
@@ -1,17 +1,90 @@
+/* ------------------------------ Dependencies ------------------------------ */
+
 const express = require('express');
 const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const moment = require('moment');
+
+/* ---------------------------- Global Variables ---------------------------- */
 
 const app = express();
 const port = 3000;
-const connection = mysql.createConnection({
+
+const db = mysql.createConnection({
     host: 'localhost',
-    user: 'admin',
-    password: 'admin',
+    user: 'root',
+    password: 'password',
     database: 'wbas_mysql',
 });
 
-connection.connect(() => console.log('mysql conntected'));
+/* ----------------------------- Initialization ----------------------------- */
 
-app.get('/', (req, res) => res.send('Hello World!'));
+db.connect(err => {
+    if (err) {
+        throw err;
+    }
+    console.log('MySql Connected...');
+});
 
-app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(port, () => console.log(`listening at http://localhost:${port}/`));
+app.use(express.static('docs'));
+
+/* ------------------------- Query Helper Functions ------------------------- */
+
+const getFirstAvailableSlot = (parkingType, callback) => {
+    db.query(
+        'SELECT * FROM lot WHERE lot_type = ? AND is_available = 1 ORDER BY lot_id ASC LIMIT 1',
+        parkingType,
+        (error, results) => {
+            if (error) throw error;
+            callback(results[0].lot_id);
+        }
+    );
+    db.query(
+        'UPDATE lot SET is_available = 0 WHERE lot_type = ? AND is_available = 1 ORDER BY lot_id ASC LIMIT 1',
+        parkingType,
+        (error, results) => {
+            if (error) throw error;
+        }
+    );
+};
+
+const ticketPost = ticketPostData => {
+    console.log(ticketPostData);
+    db.query('INSERT INTO vehicle SET VIN=?', ticketPostData.VIN, function(error, results) {
+        if (error) throw error;
+    });
+    db.query('INSERT INTO parking_ticket SET ?', ticketPostData, function(error, results) {
+        if (error) throw error;
+    });
+};
+
+/* --------------------------------- Routing -------------------------------- */
+
+app.post('/handleTicket', (req, res) => {
+    const { VIN } = req.body;
+    const { parkingType } = req.body;
+    let rateCode;
+    if (parkingType === 'covered') {
+        rateCode = 'CP';
+    } else {
+        rateCode = 'UP';
+    }
+    const today = moment().format('YYYY-MM-DD');
+    const time = moment().format('hh:mm:ss');
+    getFirstAvailableSlot(parkingType, function(parkingSpot) {
+        const ticketPostData = {
+            check_in_time: time,
+            check_in_date: today,
+            rate_code: rateCode,
+            lot_id: parkingSpot,
+            VIN,
+        };
+        ticketPost(ticketPostData);
+        res.json(ticketPostData);
+        res.end();
+    });
+});
